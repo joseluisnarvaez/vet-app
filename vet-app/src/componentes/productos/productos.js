a52651d32a2f872dffdb5100b7ed881da13edd39
Jose Narvaez <joseluis.narvaez0@gmail.com>
2023-09-16 00:53:58 -0300

    Cambios componenete tabla

--- vet-app/src/componentes/productos/productos.js
+++ vet-app/src/componentes/productos/productos.js
@@ -2,42 +2,96 @@ import React, { useState, useEffect } from 'react';
 import Tabla from '../tablas/tablaComponente';
 import axios from 'axios';
 import Menu from '../template/menu';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
-const options = [
-
-  {
-    name: 'Disable backdrop',
-    scroll: false,
-    backdrop: false,
-  },
-];
 const App = () => {
+  const [categoriaData, setCategoriaData] = useState({});
+  const [SubCategoriaData, setSubCategoriaData] = useState({});
+  const [activeTab, setActiveTab] = useState('Categoria'); // Estado para rastrear la pestaña activa
+  const [productosData, setProductosData] = useState({}); 
 
-  const [categoriaData, setCategoriaData] = useState([]); // Estado para almacenar los datos de la categoría
   const headers = {
     'Authorization': localStorage.getItem('token')
   };
 
-  useEffect(() => {
-    // Realizar la solicitud GET cuando el componente se monte
-    axios.get('http://localhost:8080/categoria/listar', { headers })
+  const funCambioPaginaCategoria = (pagina) => {
+    axios.get(`http://localhost:8080/categoria/listar?page=${pagina}`, { headers })
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      setCategoriaData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+  const funCambioPaginaProducto = (pagina) => {
+    axios.get(`http://localhost:8080/productos/listar?page=${pagina}`, { headers })
       .then(function (response) {
-        // Actualizar el estado con los datos de la categoría cuando la solicitud se complete
-        setCategoriaData(response.data.listaCategorias);
+        setProductosData(response.data);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }, []); // El array vacío [] asegura que esto solo se ejecute una vez al montar el componente
+  };
+  const funCambioPaginaSubCategoria = (pagina) => {
+    axios.get(`http://localhost:8080/subcategorias/listar${pagina}`, { headers })
+    .then(function (response) {
+      setSubCategoriaData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+  useEffect(() => {
+    // Realiza la solicitud GET cuando el componente se monte
+    axios.get('http://localhost:8080/categoria/listar', { headers })
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        setCategoriaData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    axios.get('http://localhost:8080/subcategorias/listar', { headers })
+      .then(function (response) {
+        setSubCategoriaData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      axios.get('http://localhost:8080/productos/listar', { headers })
+      .then(function (response) {
+        setProductosData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    
-      <>  
-        <Menu></Menu>
-         <Tabla data={categoriaData}></Tabla>
+    <>
+      <Menu />
 
+      <Tabs
+        defaultActiveKey="Categoria"
+        activeKey={activeTab} // Usa el estado activeTab para rastrear la pestaña activa
+        onSelect={(tab) => setActiveTab(tab)} // Actualiza el estado cuando cambia la pestaña activa
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="Categoria" title="Categoria">
+          {activeTab === 'Categoria' && <Tabla data={categoriaData.listaCategorias} {...categoriaData} {...funCambioPaginaCategoria}/>}
+        </Tab>
+        <Tab eventKey="SubCategoria" title="SubCategoria">
+          {activeTab === 'SubCategoria' && <Tabla data={SubCategoriaData.listaSubCategorias} {...categoriaData} {...funCambioPaginaSubCategoria} />}
+        </Tab>
+        <Tab eventKey="Productos" title="Productos">
+          {activeTab === 'Productos' && <Tabla data={productosData.listaProductos} {...productosData} {...funCambioPaginaProducto} />}
+        </Tab>
+      </Tabs>
     </>
-
   );
 };
 
--- vet-app/src/componentes/tablas/tablaComponente.js
+++ vet-app/src/componentes/tablas/tablaComponente.js
@@ -1,32 +1,66 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
 
-const Tabla = ({ data }) => {
+const Tabla = ({ data, paginaActual, cantidadPaginas, cantidadPorPagina , funCambioPagina}) => {
   if (!data || data.length === 0) {
     return <p>No hay datos para mostrar.</p>;
   }
 
   const headers = Object.keys(data[0]);
 
+  // Calcula la página anterior y siguiente
+  const paginaAnterior = paginaActual > 1 ? paginaActual - 1 : 1;
+  const paginaSiguiente = paginaActual < cantidadPaginas ? paginaActual + 1 : cantidadPaginas;
+
+  // Calcula los índices para mostrar los datos de la página actual
+  const startIndex = (paginaActual - 1) * cantidadPorPagina;
+  const endIndex = Math.min(startIndex + cantidadPorPagina, data.length);
+
+  // Filtra los datos para mostrar solo los de la página actual
+  const datosPaginados = data.slice(startIndex, endIndex);
+
+  const handlePaginaClick = (pagina) => {
+    funCambioPagina(pagina);
+    console.log(`Cambiando a la página ${pagina}`);
+  };
+
   return (
-    <Table   responsive="xl" striped="columns">
-      <thead>
-        <tr>
-          {headers.map((header, index) => (
-            <th key={index}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {headers.map((header, headerIndex) => (
-              <td key={headerIndex}>{row[header]}</td>
+    <>
+      <Table responsive="xl" striped="columns">
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
             ))}
           </tr>
+        </thead>
+        <tbody>
+          {datosPaginados.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {headers.map((header, headerIndex) => (
+                <td key={headerIndex}>{row[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Pagination>
+        <Pagination.First onClick={() => handlePaginaClick(1)} />
+        <Pagination.Prev onClick={() => handlePaginaClick(paginaAnterior)} />
+        {Array.from({ length: cantidadPaginas }, (_, index) => (
+          <Pagination.Item
+            key={index}
+            active={paginaActual === index + 1}
+            onClick={() => handlePaginaClick(index + 1)}
+          >
+            {index + 1}
+          </Pagination.Item>
         ))}
-      </tbody>
-    </Table>
+        <Pagination.Next onClick={() => handlePaginaClick(paginaSiguiente)} />
+        <Pagination.Last onClick={() => handlePaginaClick(cantidadPaginas)} />
+      </Pagination>
+    </>
   );
 };
 
