5e8c2175fac31477c2c55fb921f1fa7e974a86f6
Jose Narvaez <jnarvaeg@ext.sonda.com>
2024-02-24 01:03:35 -0300

    Correccion de build

--- vet-app/src/App.js
+++ vet-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 import Routers from './Routers';
 import Dashboard from './pages/Dashboard';
 import Login from './components/login/login';
--- vet-app/src/Routers.js
+++ vet-app/src/Routers.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginComponent from "./components/login/login";
